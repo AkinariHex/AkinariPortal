8538c29eca63481eb831ab47e79cc1c34a482f51
AkinariHex <akinari@outlook.it>
2022-08-22 12:20:15 +0200

    favicon fix v2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -25,7 +25,7 @@ function MyApp({ Component, pageProps, session, userStatus }) {
   return (
     <>
       <Head>
-        <link rel="icon" type="image/ico" href="/img/favicon.ico" />
+        <link rel="icon" type="image/ico" href="favicon.ico" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* HTML */}
