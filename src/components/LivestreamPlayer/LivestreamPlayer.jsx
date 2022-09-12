0a9ccdf5998b7a3d1d570ae816357ad3fca7e34e
AkinariHex <akinari@outlook.it>
2022-09-12 17:52:42 +0200

    Secret fix

--- src/components/LivestreamPlayer/LivestreamPlayer.jsx
+++ src/components/LivestreamPlayer/LivestreamPlayer.jsx
@@ -1,31 +1,31 @@
 import { useEffect, useState } from "react";
 import ReactPlayer from "react-player/twitch";
 
-async function generateToken(channel) {
-  var response = await fetch("/api/twitch/generate");
-  response = await response.json();
-
-  if (response.message === "token created") {
-    return checkUserLivestream(channel);
-  }
-}
-
-async function checkUserLivestream(channel, setIsUserLive) {
-  var checkResponse = await fetch(
-    `/api/twitch/checklivestream?channel=${channel}&secret=${process.env.TWITCH_DB_SECRET}`
-  );
-  checkResponse = await checkResponse.json();
-  if (!checkResponse.status === 401) {
-    return generateToken(channel);
-  }
-
-  if (checkResponse.is_live === true) {
-    setIsUserLive(true);
-  }
-}
-
 function LivestreamPlayer({ twitchName }) {
   const [isUserLive, setIsUserLive] = useState(false);
+
+  async function generateToken(channel) {
+    var response = await fetch("/api/twitch/generate");
+    response = await response.json();
+
+    if (response.message === "token created") {
+      return checkUserLivestream(channel);
+    }
+  }
+
+  async function checkUserLivestream(channel, setIsUserLive) {
+    var checkResponse = await fetch(
+      `/api/twitch/checklivestream?channel=${channel}&secret=${process.env.TWITCH_DB_SECRET}`
+    );
+    checkResponse = await checkResponse.json();
+    if (!checkResponse.status === 401) {
+      return generateToken(channel);
+    }
+
+    if (checkResponse.is_live === true) {
+      setIsUserLive(true);
+    }
+  }
 
   useEffect(() => {
     checkUserLivestream(twitchName, setIsUserLive);
